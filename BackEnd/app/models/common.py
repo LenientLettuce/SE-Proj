d5161ac438066ae573b173530c760cfd8c5cd6c0
adeenahtt <163740024+adeenahtt@users.noreply.github.com>
2026-04-12 18:38:41 +0500

    fixing setup
    fixing setup

--- BackEnd/app/models/common.py
+++ BackEnd/app/models/common.py
@@ -5,24 +5,25 @@ from bson import ObjectId
 from pydantic import BaseModel, ConfigDict, Field
 
 
-class PyObjectId(str):
-    @classmethod
-    def __get_pydantic_core_schema__(cls, _source_type: Any, _handler: Any):
-        from pydantic_core import core_schema
+# class PyObjectId(str):
+#     @classmethod
+#     def __get_pydantic_core_schema__(cls, _source_type: Any, _handler: Any):
+#         from pydantic_core import core_schema
 
-        def validate(value: Any) -> str:
-            if isinstance(value, ObjectId):
-                return str(value)
-            if ObjectId.is_valid(value):
-                return str(ObjectId(value))
-            raise ValueError("Invalid ObjectId")
+#         def validate(value: Any) -> str:
+#             if isinstance(value, ObjectId):
+#                 return str(value)
+#             if ObjectId.is_valid(value):
+#                 return str(ObjectId(value))
+#             raise ValueError("Invalid ObjectId")
 
-        return core_schema.no_info_plain_validator_function(validate)
+#         return core_schema.no_info_plain_validator_function(validate)
 
 
 class MongoBaseModel(BaseModel):
     model_config = ConfigDict(populate_by_name=True)
-    id: PyObjectId | None = Field(default=None, alias="_id")
+
+    id: str | None = Field(default=None, alias="_id")
 
 
 class TimestampedModel(BaseModel):
